1cea1e17143f3af625c762a8ec8f74abbde00f6a
Adam Kopiec <adas073@gmail.com>
2024-12-19 13:47:26 +0100

    Task 2

--- main.py
+++ main.py
@@ -40,3 +40,71 @@ class BankAccount:
 		self.balance -= amount
 		print("New balance:", self.balance)
 
+class ToDoList:
+	def __init__(self, tasks):
+		self.tasks = tasks
+
+	def createTask(self, title, description, priority):
+		task = Task(title, description, priority)
+		self.tasks[title] = task
+		print('Task added to the list')
+
+	def completeTask(self, title):
+		task = self.tasks[title]
+		task.markCompletion()
+
+	def deleteTask(self, title):
+		del self.tasks[title]
+		print('Task deleted')
+	
+	def viewTasks(self):
+		for key, value in self.tasks.items():
+			print(value.display())
+
+	def viewSortedTasksByPriority(self):
+		sortedTasks = dict(sorted(self.tasks.items(), key=lambda x: x[1].priority, reverse=True))
+		for key, value in sortedTasks.items():
+			print(value.display())
+
+	def viewCompletedTasks(self):
+		for key, value in self.tasks.items():
+			if value.isCompleted():
+				print(value.display())
+
+class Task:
+	def __init__(self, title, description, priority):
+		self.title = title
+		self.description = description
+		self.priority = priority
+		self.completed = False
+
+	def markCompletion(self):
+		self.completed = True
+		print('Task completed')
+
+	def display(self):
+		print('Task')
+		print(self.title)
+		print(self.description)
+		print(self.priority)
+		print(self.completed)
+
+	def isCompleted(self):
+		return self.completed
+	
+
+task1 = Task('Task1', 'Description1', 1)
+task2 = Task('Task2', 'Description2', 2)
+
+tasks = {}
+tasks[task1.title] = task1
+tasks[task2.title] = task2
+
+toDoList = ToDoList(tasks)
+toDoList.createTask('Task3', 'Description3', 3)
+toDoList.viewTasks()
+
+toDoList.completeTask('Task1')
+toDoList.viewCompletedTasks()
+
+toDoList.viewSortedTasksByPriority()
